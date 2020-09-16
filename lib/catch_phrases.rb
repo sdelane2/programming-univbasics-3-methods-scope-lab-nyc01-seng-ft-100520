384bb9089cab60ff98787ff949aae21a0363dab4
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-16 05:44:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,15 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  saying = "It's Dangerous To Go Alone! Take This."
+  puts saying
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
+end
+  
